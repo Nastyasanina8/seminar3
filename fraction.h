b0a227723231a06018d2e9b93c9cb4908c5d20f8
Санина Настя <8nastyasanina@gmail.com>
2025-09-22 21:29:01 +0300

    Попытка 1

---
+++ fraction.h
@@ -0,0 +1,32 @@
+#ifndef FRACTION_H
+
+   #define FRACTION_H
+
+      struct Fraction {
+
+       int num; // числитель
+
+       int den; // знаменатель
+
+   };
+
+/**
+ * @brief Складывает две дроби
+
+ * @param a Первая дробь для сложения
+
+ * @param b Вторая дробь для сложения
+
+ * @return Новая дробь, являющаяся результатом сложения
+
+ */
+
+      // Объявляем наши функции
+
+   Fraction add(Fraction a, Fraction b);
+
+   Fraction multiply(Fraction a, Fraction b);
+
+   void simplify(Fraction &f); // упрощаем переданную дробь
+
+      #endif
